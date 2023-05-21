e6cb4656e9a682f7f5d1d821b1712c3f41f3c291
Thiago Sousa <thiagosousa81@gmail.com>
2023-05-20 21:35:47 -0300

    Add files via upload

---
+++ htdocs/script.js
@@ -0,0 +1,11 @@
+function changeButtonColorOnMouseOver() {
+  var button = document.getElementById('button');
+  button.style.backgroundColor = 'white';
+  button.style.color = 'black';
+}
+
+function changeButtonColorOnMouseOut() {
+  var button = document.getElementById('button');
+  button.style.backgroundColor = '#ffd043';
+  button.style.color = 'black';
+}
